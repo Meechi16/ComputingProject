0f22e035b2eae607f7c8cc069d8956ea135b6d02
Unknown <amechidawudawodu@gmail.com>
2018-02-28 23:25:44 +0000

    I got the subject list to partly work

--- DBPrototype1/DBPrototype1/Subjectlist.Designer.vb
+++ DBPrototype1/DBPrototype1/Subjectlist.Designer.vb
@@ -23,33 +23,88 @@ Partial Class Subjectlist
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.NamesofStudents = New System.Windows.Forms.DataGridView()
-        Me.Button1 = New System.Windows.Forms.Button()
+        Me.LoadTable = New System.Windows.Forms.Button()
+        Me.SeatingButt = New System.Windows.Forms.Button()
+        Me.Addbutt = New System.Windows.Forms.Button()
+        Me.DeleteButt = New System.Windows.Forms.Button()
+        Me.UpdateButt = New System.Windows.Forms.Button()
+        Me.EditButt = New System.Windows.Forms.Button()
         CType(Me.NamesofStudents, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'NamesofStudents
         '
         Me.NamesofStudents.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.NamesofStudents.Location = New System.Drawing.Point(25, 42)
+        Me.NamesofStudents.Location = New System.Drawing.Point(39, 12)
         Me.NamesofStudents.Name = "NamesofStudents"
         Me.NamesofStudents.Size = New System.Drawing.Size(401, 232)
         Me.NamesofStudents.TabIndex = 0
         '
-        'Button1
+        'LoadTable
         '
-        Me.Button1.Location = New System.Drawing.Point(189, 281)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
-        Me.Button1.TabIndex = 1
-        Me.Button1.Text = "Button1"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.LoadTable.Location = New System.Drawing.Point(182, 281)
+        Me.LoadTable.Name = "LoadTable"
+        Me.LoadTable.Size = New System.Drawing.Size(75, 23)
+        Me.LoadTable.TabIndex = 1
+        Me.LoadTable.Text = "Load Table"
+        Me.LoadTable.UseVisualStyleBackColor = True
+        '
+        'SeatingButt
+        '
+        Me.SeatingButt.Location = New System.Drawing.Point(328, 281)
+        Me.SeatingButt.Name = "SeatingButt"
+        Me.SeatingButt.Size = New System.Drawing.Size(75, 23)
+        Me.SeatingButt.TabIndex = 2
+        Me.SeatingButt.Text = "Seating Plan"
+        Me.SeatingButt.UseVisualStyleBackColor = True
+        '
+        'Addbutt
+        '
+        Me.Addbutt.Location = New System.Drawing.Point(39, 281)
+        Me.Addbutt.Name = "Addbutt"
+        Me.Addbutt.Size = New System.Drawing.Size(75, 23)
+        Me.Addbutt.TabIndex = 3
+        Me.Addbutt.Text = "Add"
+        Me.Addbutt.UseVisualStyleBackColor = True
+        '
+        'DeleteButt
+        '
+        Me.DeleteButt.Location = New System.Drawing.Point(39, 250)
+        Me.DeleteButt.Name = "DeleteButt"
+        Me.DeleteButt.Size = New System.Drawing.Size(75, 23)
+        Me.DeleteButt.TabIndex = 4
+        Me.DeleteButt.Text = "Delete"
+        Me.DeleteButt.UseVisualStyleBackColor = True
+        '
+        'UpdateButt
+        '
+        Me.UpdateButt.Location = New System.Drawing.Point(182, 250)
+        Me.UpdateButt.Name = "UpdateButt"
+        Me.UpdateButt.Size = New System.Drawing.Size(75, 23)
+        Me.UpdateButt.TabIndex = 5
+        Me.UpdateButt.Text = "Update"
+        Me.UpdateButt.UseVisualStyleBackColor = True
+        '
+        'EditButt
+        '
+        Me.EditButt.Location = New System.Drawing.Point(328, 250)
+        Me.EditButt.Name = "EditButt"
+        Me.EditButt.Size = New System.Drawing.Size(75, 23)
+        Me.EditButt.TabIndex = 6
+        Me.EditButt.Text = "Edit"
+        Me.EditButt.UseVisualStyleBackColor = True
         '
         'Subjectlist
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(482, 316)
-        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.EditButt)
+        Me.Controls.Add(Me.UpdateButt)
+        Me.Controls.Add(Me.DeleteButt)
+        Me.Controls.Add(Me.Addbutt)
+        Me.Controls.Add(Me.SeatingButt)
+        Me.Controls.Add(Me.LoadTable)
         Me.Controls.Add(Me.NamesofStudents)
         Me.Name = "Subjectlist"
         Me.Text = "Subjectlist"
@@ -58,5 +113,10 @@ Partial Class Subjectlist
 
     End Sub
     Friend WithEvents NamesofStudents As System.Windows.Forms.DataGridView
-    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents LoadTable As System.Windows.Forms.Button
+    Friend WithEvents SeatingButt As System.Windows.Forms.Button
+    Friend WithEvents Addbutt As System.Windows.Forms.Button
+    Friend WithEvents DeleteButt As System.Windows.Forms.Button
+    Friend WithEvents UpdateButt As System.Windows.Forms.Button
+    Friend WithEvents EditButt As System.Windows.Forms.Button
 End Class
--- DBPrototype1/DBPrototype1/Subjectlist.vb
+++ DBPrototype1/DBPrototype1/Subjectlist.vb
@@ -4,20 +4,26 @@ Public Class Subjectlist
     Dim SDA As New OleDbDataAdapter
     Dim DbDataSet As New DataTable
     Dim BSource As New BindingSource
+    Dim form1 As Form1
 
-    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles LoadTable.Click
         Dim provider = "Provider=Microsoft.ACE.OLEDB.12.0;Data source="
         Dim datafile = "login form.accdb"
         Dim connectstring = provider & datafile ' telling the program wich file to look in
         Dim myconnection = New OleDbConnection
         myconnection.ConnectionString = connectstring ' establishing the connection string 
         myconnection.Open() ' open connection
-        Dim query As String = "SELECT * FROM Classes"
-        Dim commander = New OleDbCommand(query, myconnection)
+        Dim PickTheRightStudents1 As String = "SELECT * FROM TakenClass INNER JOIN StudentInfo ON TakenClass.StudentID = StudentInfo.StudentID WHERE TakenClass.ClassID = 'Eng12'"
+        Dim PickTheRightStudents2 As String = "SELECT Firstname, LastName FROM TakenClass INNER JOIN StudentInfo ON TakenClass.StudentID = StudentInfo.StudentID WHERE TakenClass.ClassID = Mat12 "
+        Dim commander = New OleDbCommand(PickTheRightStudents1, myconnection)
         SDA.SelectCommand = commander
         SDA.Fill(DbDataSet)
         BSource.DataSource = DbDataSet
         NamesofStudents.DataSource = BSource
         SDA.Update(DbDataSet)
+        myconnection.Close()
     End Sub
+
+    
 End Class
